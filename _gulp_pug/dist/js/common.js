e42462763873af90ee748f8b7a05622521013ba8
Dmitriy Khristoev <haossut@gmail.com>
2019-03-18 14:24:49 +0200

    new styles

--- _gulp_pug/dist/js/common.js
+++ _gulp_pug/dist/js/common.js
@@ -14,76 +14,8 @@ $(document).on('ready', function(){
   } catch(err) {
 
   };
-
-  // simpleForm version 2015-09-23 14:30 GMT +2
-  simpleForm('form.form-callback');
 });
 
-$(window).on('load', function() {
-  $(".loader").delay(400).fadeOut("slow");
-});
-
+$(window).on('load', function() { });
 $(window).on('scroll', function() { });
 $(window).on('resize', function() { });
-
-/*
-version 2015-09-23 14:30 GMT +2
-*/
-function simpleForm(form, callback) {
-  $(document).on('submit', form, function(e){
-    e.preventDefault();
-    var formData = {};
-    var hasFile = false;
-    if ($(this).find('[type=file]').length < 1) {
-      formData = $(this).serialize();
-    }
-    else {
-      formData = new FormData();
-      $(this).find('[name]').each(function(){
-
-        switch($(this).prop('type')) {
-
-          case 'file':
-            if ($(this)[0]['files'].length > 0) {
-              formData.append($(this).prop('name'), $(this)[0]['files'][0]);
-              hasFile = true;
-            }
-            break;
-
-          case 'radio':
-          case 'checkbox':
-            if (!$(this).prop('checked')) {
-              break;
-            }
-            formData.append($(this).prop('name'), $(this).val().toString());
-            break;
-
-          default:
-            formData.append($(this).prop('name'), $(this).val().toString());
-            break;
-        }
-      });
-    }
-
-    $.ajax({
-      url: $(this).prop('action'),
-      data: formData,
-      type: 'POST',
-      contentType : hasFile ? 'multipart/form-data' : 'application/x-www-form-urlencoded',
-      cache       : false,
-      processData : false,
-      success: function(response) {
-        $(form).removeClass('ajax-waiting');
-        $(form).html($(response).find(form).html());
-
-        if (typeof callback === 'function') {
-          callback(response);
-        }
-      }
-    });
-
-    $(form).addClass('ajax-waiting');
-
-    return false;
-  });
-}
