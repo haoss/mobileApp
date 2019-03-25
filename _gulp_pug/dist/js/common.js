5a28cc21d772594c8f2caf16ba52ab82cce82d43
Dmitriy Khristoev <haossut@gmail.com>
2019-03-25 14:32:32 +0200

    new styles

--- _gulp_pug/dist/js/common.js
+++ _gulp_pug/dist/js/common.js
@@ -5,6 +5,16 @@ $(document).on('ready', function(){
 
   $('.svg-to-inline').svgToInline();
 
+  $('.users-say__carousel').slick({
+    variableWidth: true,
+    centerMode: true,
+    slidesToShow: 3,
+    infinite: true,
+    dots: true,
+    prevArrow: '<button type="button" class="slick-prev"><img src="./images/icon/icon-arrow-prev.svg" alt=""/></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="./images/icon/icon-arrow-next.svg" alt=""/></button>'
+  });
+
   // Chrome Smooth Scroll
   try {
     $.browserSelector();
